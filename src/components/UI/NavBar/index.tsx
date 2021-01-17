46568ab6ebdda66df7e9e91f174354bc7adcd078
Lukas <stratocaster@live.dk>
2021-01-17 16:37:13 +0100

    Adding login, testing api

--- src/components/UI/NavBar/index.tsx
+++ src/components/UI/NavBar/index.tsx
@@ -1,63 +1,89 @@
-import React, { useState } from 'react';
-import { Column } from '../../Common/components/grid';
-import { NavbarContainer } from './components/navbarContainer';
-import { NavigationLink } from './components/navigationLink';
+import React, { useState } from "react";
+import { Column } from "../../Common/components/grid";
+import { NavbarContainer } from "./components/navbarContainer";
+import { NavigationLink } from "./components/navigationLink";
 
-import { Button } from '../../Common/components/button';
-import { Icon } from '../../Common/components/icon';
-import Axios from 'axios';
-import { NavLink } from 'react-router-dom';
+import { Button } from "../../Common/components/button";
+import { Icon } from "../../Common/components/icon";
+import Axios from "axios";
+import { NavLink } from "react-router-dom";
+import useForm from "../../../hooks/useForm";
 
+export interface NavBarProps {}
 
+const NavBar: React.FC<NavBarProps> = () => {
+  const [firstName, setFirstName] = useState<string | undefined>("");
+  const { handleInputChange, data } = useForm({ password: "", username: "" });
 
-export interface NavBarProps {
-    
-}
- 
-const NavBar: React.SFC<NavBarProps> = () => {
-    const [firstName, setFirstName] = useState<string | undefined>('')
-
-    const handleClick = () => {
-        Axios.post<{userId: string}>(`/api/MakeUser`, {
-            firstName: 'Fred',
-            lastName: 'Flintstone',
-            email:'flintstone@example.com',
-            password: 'Sommer123!'
-          })
-  .then((response) => {
-    // handle success
-    Axios.get<{
-        firstName: string,
-        lastName: string,
-        lastLogin: string
-    }>(`/api/UserDetails?userId=${response.data}`).then(response => {
-setFirstName(response.data?.firstName)
+  const onSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    console.log(data);
+    event.preventDefault();
+    Axios.post<{ access_token: string }>(
+      `https://nosetest.herokuapp.com/auth`,
+      {
+        username: data.username,
+        password: data.password,
+      }
+    )
+      .then((response) => {
+        // handle success
+        localStorage.setItem("token", response.data.access_token);
+        setFirstName("success");
+        /*
+        Axios.get<{
+          firstName: string;
+          lastName: string;
+          lastLogin: string;
+        }>(`/api/UserDetails?userId=${response.data}`).then((response) => {
+          setFirstName(response.data?.firstName);
+        });
+        */
       })
-  })
-  .catch((error) => {
-    // handle error
-    console.log(error); 
-  })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  };
+  return (
+    <Column>
+      <NavbarContainer>
+        <NavigationLink to="/">Home</NavigationLink>
+        <NavigationLink to="/reviews">Anmeldelser</NavigationLink>
+        <NavigationLink to="/blog">Blog</NavigationLink>
+        <NavigationLink to="/om">Om</NavigationLink>
+        <NavLink to="/createreview">
+          <Button>Anmeld produkt</Button>
+        </NavLink>
+        {firstName && (
+          <span style={{ fontWeight: "bold", fontSize: "20px" }}>
+            {firstName}
+          </span>
+        )}
 
-    }
-    return ( 
-     <Column>
-        <NavbarContainer>
+        {localStorage.getItem("user") ? (
+          <Button backgroundColor="transparent">
+            <Icon icon="user-circle" />
+          </Button>
+        ) : (
+          <form style={{ display: "inline-block" }} onSubmit={onSubmit}>
+            <input
+              name="username"
+              onChange={handleInputChange}
+              placeholder="username"
+              type="text"
+            ></input>
+            <input
+              name="password"
+              onChange={handleInputChange}
+              placeholder="password"
+              type="password"
+            ></input>
+            <input type="submit" value="Login" />
+          </form>
+        )}
+      </NavbarContainer>
+    </Column>
+  );
+};
 
-
-            <NavigationLink to='/'>Home</NavigationLink>
-<NavigationLink to='/reviews'>Anmeldelser</NavigationLink>
-<NavigationLink to='/blog'>Blog</NavigationLink>
-<NavigationLink to='/om'>Om</NavigationLink>
-<NavLink to='/createreview'><Button>Anmeld produkt</Button></NavLink>
-{firstName && <span style={{fontWeight: 'bold', fontSize: '20px'}}>{firstName}</span>}
-<Button onClick={handleClick} backgroundColor='transparent'>
-<Icon icon='user-circle' />
-</Button>
-
-        </NavbarContainer>
-        </Column>
-     );
-}
- 
 export default NavBar;
--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,18 +1,68 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react";
 
+const useForm = (initialState: any) => {
+  const [data, setData] = useState(initialState);
+  const [formDisabled, setFormDisabled] = useState(true);
 
-const useForm = () => {
-    const [values, setValues] = useState<{[key: string]: any}>({})
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const name = event?.target.name;
+    const value = event?.target.value;
+    const copiedData = { ...data };
+    copiedData[name] = value;
+    setData(copiedData);
+  };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        console.log('test')
-        const name = event.target.name;
-        const value = event.target.value;
-        setValues(prevValues => { return {...prevValues, [name]: value}})
-    }
+  const handleSelectChange = (
+    event: ChangeEvent<{
+      name?: string | undefined;
+      value: unknown;
+    }>
+  ) => {
+    const name = event?.target.name;
+    const value = event?.target.value;
+    const copiedData = { ...data };
+    if (name) copiedData[name] = value;
+    setData(copiedData);
+  };
 
-    return {values, handleChange}
-    
-}
+  const handleDateChange = (date: Date | null, fieldName: string) => {
+    const name = fieldName;
+    const value = date;
+    const copiedData = { ...data };
+    copiedData[name] = value;
+    setData(copiedData);
+  };
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const name = event?.target.name;
+    const value = event?.target.checked;
+    const copiedData = { ...data };
+    copiedData[name] = value;
+    setData(copiedData);
+  };
+
+  const enableForm = () => {
+    setFormDisabled(false);
+  };
+
+  const disableForm = () => {
+    setFormDisabled(true);
+  };
+
+  const resetForm = () => {
+    setData(initialState);
+  };
+
+  return {
+    data,
+    handleInputChange,
+    handleSelectChange,
+    handleDateChange,
+    handleCheckboxChange,
+    formDisabled,
+    enableForm,
+    disableForm,
+    resetForm,
+  };
+};
 
 export default useForm;
